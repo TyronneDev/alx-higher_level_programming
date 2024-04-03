da84fba967e177cdfa1ce1f0be7045d16c5415b5
TyronneDev <tyronne.j@outlook.com>
2024-04-03 05:23:01 +0000

    Task Done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+class Square prints a square
+"""
+
+
+class Square:
+    """
+    class Square prints a square
+    """
+
+    def __init__(self, size):
+        """
+        Initialization function
+
+        Args:
+        param1: the size of square
+        """
+        self.__size = size
