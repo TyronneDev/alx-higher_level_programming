ac142e59dee45ea93e40d05d667f8013fa81e92d
TyronneDev <tyronne.j@outlook.com>
2024-06-07 14:36:06 +0000

    Task Done

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        return ((-number % 10) * -1)
+        result = (number % 10) * -1
     elif number >= 0:
-        return (number % 10)
+        result = number % 1)
+    print result
