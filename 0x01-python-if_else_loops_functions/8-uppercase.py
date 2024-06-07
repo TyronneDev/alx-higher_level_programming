b4200d4981e7c57c75c5ffa61034af8677979b51
TyronneDev <tyronne.j@outlook.com>
2024-06-07 13:42:33 +0000

    Task Done

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,6 @@ def uppercase(str):
         if ord(i) in range(97, 123):
             i = (ord(i)) - 32
             print("{:c}".format(i), end="")
-        else:
-            print(i, end="")
+        elif ord(i) not in range(97, 123):
+            i = ord(i)
+            print("{:c}".format(i), end="")
