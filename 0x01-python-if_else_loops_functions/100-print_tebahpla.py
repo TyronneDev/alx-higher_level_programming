e01ebe0d8aa1082e3f8a7a2312070fe5cf0c6ad8
TyronneDev <tyronne.j@outlook.com>
2024-06-08 05:34:22 +0000

    Task Done

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-def reverse_alphabets():
-    for i in range(122, 96, -1):
-        if i % 2 == 0:
-            i = i
-        elif i % 2 == 1:
-            i = i - 32
-        print("{:c}".format(i), end="")
+for i in range(122, 96, -1):
+    if i % 2 == 0:
+        i = i
+    elif i % 2 == 1:
+        i = i - 32
+    print("{:c}".format(i), end="")
