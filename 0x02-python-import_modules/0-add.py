3e2091aaa043cc11eb12762a1c978b2879dd2e47
TyronneDev <tyronne.j@outlook.com>
2024-06-09 05:23:30 +0000

    Task Done

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,6 +3,5 @@ if __name__ == '__main__':
     from add_0 import add
 
     a = 1
-    b = 2
-    sum = add(a, b)
-    print("{:d} + {:d} = {:d}".format(a, b, sum))
+    b = 2 
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
