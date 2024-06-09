ddb16068e83bd003976e2fc4c3a97a21ea67c8f2
TyronneDev <tyronne.j@outlook.com>
2024-06-09 15:30:33 +0000

    Task Done

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -13,13 +13,13 @@ if __name__ == "__main__":
     else:
         if argv[2] == '+':
             print("{} {} {} = {}".format(argv[1], argv[2], argv[3],
-                  add(int(argv[1]), int(argv[3]))))
+                  calc.add(int(argv[1]), int(argv[3]))))
         elif argv[2] == '-':
             print("{} {} {} = {}".format(argv[1], argv[2], argv[3],
-                  sub(int(argv[1]), int(argv[3]))))
+                  calc.sub(int(argv[1]), int(argv[3]))))
         elif argv[2] == '*':
             print("{} {} {} = {}".format(argv[1], argv[2], argv[3],
-                  mul(int(argv[1]), int(argv[3]))))
+                  calc.mul(int(argv[1]), int(argv[3]))))
         elif argv[2] == '/':
             print("{} {} {} = {}".format(argv[1], argv[2], argv[3],
-                  div(int(argv[1]), int(argv[3]))))
+                  calc.div(int(argv[1]), int(argv[3]))))
