cfffd6105f7afefdb60554d46e5fa463639bdc1e
TyronneDev <tyronne.j@outlook.com>
2024-06-09 07:26:21 +0000

    Task Done

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,9 +5,10 @@ if __name__ == "__main__":
     sum = 0
     if len(argv) - 1 < 1:
         sum = 0
+        print(sum)
     else:
         args = int(argv[1:])
         sum = 0
         for i in args:
             sum += i
-    print(sum)
+        print(sum)
