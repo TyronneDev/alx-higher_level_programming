936b0fab3703636caa0dafdaeac87af31b614be0
TyronneDev <tyronne.j@outlook.com>
2024-06-09 07:39:30 +0000

    Task Done

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
 
     sum = 0
-    if len(argv) - 1 < 1:
+    if len(argv) - 1 = 0:
         print("{}".format(sum))
     else:
         for i in range(1, len(argv)):
