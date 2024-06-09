ba285015bd37a958f0e235e328e42ea677eeb1a2
TyronneDev <tyronne.j@outlook.com>
2024-06-09 06:42:00 +0000

    Task Done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,17 +2,13 @@
 if __name__ == "__main__":
     from sys import argv
 
-   
     print("{:d} argument".format(len(argv) - 1), end="")
     if len(argv) - 1 == 0:
         print("s.")
     else:
         if len(argv) - 1 == 1:
-            print(".")
+            print(":")
         elif len(argv) - 1 > 1:
-            print("s.")
+            print("s:")
         for idx in range(1, len(argv)):
-            print("{}: {}".format(idx, argv[idx]))
-
-
-
+            print("{:d}: {}".format(idx, argv[idx]))
