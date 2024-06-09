36e3bc94600442cc156660ef6cb9cff36a2458a6
TyronneDev <tyronne.j@outlook.com>
2024-06-09 15:52:03 +0000

    Revert "Task Done"
    This reverts commit e33d906167876c879a663815c8be5cff23a8b70d.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,18 +7,19 @@ if __name__ == "__main__":
     if len(argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif argv[2] == '+':
-        print("{} {} {} = {}".format(argv[1], argv[2], argv[3],
-              calc.add(int(argv[1]), int(argv[3]))))
-    elif argv[2] == '-':
-        print("{} {} {} = {}".format(argv[1], argv[2], argv[3],
-              calc.sub(int(argv[1]), int(argv[3]))))
-    elif argv[2] == '*':
-        print("{} {} {} = {}".format(argv[1], argv[2], argv[3],
-              calc.mul(int(argv[1]), int(argv[3]))))
-    elif argv[2] == '/':
-        print("{} {} {} = {}".format(argv[1], argv[2], argv[3],
-              calc.div(int(argv[1]), int(argv[3]))))
     elif argv[2] not in ('+', '-', '*', '/'):
         print("Unknown operator. Available operators: +, _, *, and /")
         exit(1)
+    else:
+        if argv[2] == '+':
+            print("{} {} {} = {}".format(argv[1], argv[2], argv[3],
+                  calc.add(int(argv[1]), int(argv[3]))))
+        elif argv[2] == '-':
+            print("{} {} {} = {}".format(argv[1], argv[2], argv[3],
+                  calc.sub(int(argv[1]), int(argv[3]))))
+        elif argv[2] == '*':
+            print("{} {} {} = {}".format(argv[1], argv[2], argv[3],
+                  calc.mul(int(argv[1]), int(argv[3]))))
+        elif argv[2] == '/':
+            print("{} {} {} = {}".format(argv[1], argv[2], argv[3],
+                  calc.div(int(argv[1]), int(argv[3]))))
